5b819068f3956828d9c1bfe60d0cd29deedf195d
Aadil Qureshi <36612205+aadilq@users.noreply.github.com>
2025-06-27 18:21:26 -0400

    link UI elements to ForecastViewController

--- ForecastViewController.swift
+++ ForecastViewController.swift
@@ -8,22 +8,25 @@
 import UIKit
 
 class ForecastViewController: UIViewController {
-
+    @IBOutlet weak var locationLabel: UILabel!
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    @IBOutlet weak var forecastImageView: UIImageView!
+    
+    
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    
+    @IBOutlet weak var temperatureLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+
     }
     
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
